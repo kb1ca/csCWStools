7b6bcadd3dc217333e233b9d3e1eba600c1f66a3
Kris Boutilier <kris.boutilier@gmail.com>
2015-06-04 20:44:19 -0700

    Formatting, comments.

--- csCWStools/src/ca/kb1/csCWStools/AddNodeRightACL.java
+++ csCWStools/src/ca/kb1/csCWStools/AddNodeRightACL.java
@@ -11,16 +11,20 @@ import com.opentext.livelink.service.docman.NodeRights;
 
 public class AddNodeRightACL {
 
+	// somewhere to efficiently track the users who we're targeting to be updated or added
 	static class RightID
 	{
-	    private long rightID;
+		// what is the Content Server ID for the user who's ACL we're targeting?
+		private long rightID;
+		
+		// did we manage to update an existing ACL for this user, or do we need to add a new one?
 	    private boolean updated;
 	    
 	    public RightID(long rightID) {
 	    	this.rightID = rightID;
 	    	this.updated = false;
 	    }
-	    
+
 	    public long getRightID() {
 	    	return this.rightID;
 	    }
@@ -40,29 +44,30 @@ public class AddNodeRightACL {
 		// reach out to the server and get the NodeRights associated with this Node
 	    NodeRights thisNodeRights = docManClient.getNodeRights(thisNode.getID());
 
-	    //-----			// extract the set of additional ACL's rights to this Node
+	    // extract the set of Assigned Access ACLs associated with this Node
+	    // note: a Content Server node also has a mandatory set of Owner permissions, an 
+	    //		 optional Owner Group with permissions, and optional Public Access 
+	    //		 permissions, in addition to these zero or more discrete user/group ACLs
 	    List<NodeRight> thisNodeACLRights = thisNodeRights.getACLRights();
 
-//	    System.out.println("| [" + thisNode.getID() + "] thisNodeACLRights.size()=" + thisNodeACLRights.size());
-
+	    // the usual arrangement for a 'Content Owner'
 	    NodePermissions newNodePermissions = new NodePermissions();
-	    // the usual setup for Content Owner
 	    newNodePermissions.setSeePermission(true);
 	    newNodePermissions.setSeeContentsPermission(true);
 	    newNodePermissions.setModifyPermission(true);
 	    newNodePermissions.setEditAttributesPermission(true);
 	    newNodePermissions.setAddItemsPermission(true);
 	    newNodePermissions.setReservePermission(true);
-	    newNodePermissions.setDeletePermission(true);
 	    newNodePermissions.setDeleteVersionsPermission(true);
+	    newNodePermissions.setDeletePermission(true);
 	    newNodePermissions.setEditPermissionsPermission(false);
 
+	    // define some users we want to update/add ACLs for
 	    ArrayList<RightID> targetRightIDs = new ArrayList<RightID>();
-
 	    targetRightIDs.add(new RightID(47815));
 	    targetRightIDs.add(new RightID(47816));
 
-	    // if the ID already has permissions on the node, reset them
+	    // if an ID already has some permissions on the node, reset them
 		for (NodeRight thisNodeACLRight : thisNodeACLRights) {
 			for (RightID thisTargetRightID : targetRightIDs) {
 				if (thisTargetRightID.getRightID() == thisNodeACLRight.getRightID()) {
@@ -73,12 +78,12 @@ public class AddNodeRightACL {
 			}
 		}
 
-		// attach the bits to a new user ACL
+		// otherwise attach the NodePermissions bits to a new user ACL
 	    NodeRight newNodeACLRight = new NodeRight();
 	    newNodeACLRight.setType("ACL");
 	    newNodeACLRight.setPermissions(newNodePermissions);
 
-	    // and add the user ACL for each of the users if it wasn't already there to update
+	    // and then add a user ACL for each of the users that didn't already have an ACL to update
 		for (RightID thisTargetRightID : targetRightIDs) {
 			if (!thisTargetRightID.isUpdated()) {
 				newNodeACLRight.setRightID(thisTargetRightID.getRightID());
