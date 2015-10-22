250344bd1045c343867819e05f25bbee3a6e6ee8
Kris Boutilier <kris.boutilier@gmail.com>
2015-10-22 13:42:33 -0700

    Housekeeping

--- csCWStools/src/ca/kb1/csCWStools/SOAPServices.java
+++ csCWStools/src/ca/kb1/csCWStools/SOAPServices.java
@@ -27,57 +27,59 @@ import com.opentext.livelink.service.docman.DocumentManagement_Service;
 import com.opentext.livelink.service.physobj.PhysicalObjects;
 import com.opentext.livelink.service.physobj.PhysicalObjects_Service;
 
-// follows thread-safe Singleton pattern
+// follow a thread-safe Singleton pattern
 public class SOAPServices {
-
-	// this class stores a collection of instances of Content Server CWS SOAP services
+	// this class stores a collection of references to Content Server CWS SOAP services
+	
+	// TODO need to add logic to refresh the authentication token from time to time (before it expires), but not with every request as it's an expensive operation
+	
 	private static SOAPServices instance;
-
+	
 	private SOAPServices(){}
 	
-    public static synchronized SOAPServices getInstance(){
-        if(instance == null){
-            instance = new SOAPServices();
-        }
-        return instance;
-    }
-    
+	public static synchronized SOAPServices getInstance(){
+		if(instance == null){
+			instance = new SOAPServices();
+		}
+		return instance;
+	}
+	
 	// Constructor
 	public void init(String USERNAME, String PASSWORD, List<String> SOAPServers ) throws MalformedURLException {
 		// we assume that the 1st SOAPServer is going to provide authentication
 		Authentication authClient = (new Authentication_Service(new URL( SOAPServers.get(0) + "Authentication.svc"))).getBasicHttpBindingAuthentication();
-
+		
 		// Call the AuthenticateUser() method to get the authentication token
 		try
 		{
 			System.out.print("Authenticating User...");
 			this.authToken = authClient.authenticateUser(USERNAME, PASSWORD);
 			System.out.println("SUCCESS! [got " + this.authToken + " ]\n");
-
+			
 			// Create the OTAuthentication object and set the authentication token obtained above
 			OTAuthentication otAuth = new OTAuthentication();
 			otAuth.setAuthenticationToken(this.authToken);
-
+			
 			// Create a SOAP header
 			SOAPHeader header = MessageFactory.newInstance().createMessage().getSOAPPart().getEnvelope().getHeader();
-
+			
 			// Add the OTAuthentication SOAP header element
 			SOAPHeaderElement otAuthElement = header.addHeaderElement(new QName(ECM_API_NAMESPACE, "OTAuthentication"));
-
+			
 			// Add the AuthenticationToken SOAP element
 			SOAPElement authTokenElement = otAuthElement.addChildElement(new QName(ECM_API_NAMESPACE, "AuthenticationToken"));
 			authTokenElement.addTextNode(otAuth.getAuthenticationToken());
 			
-		
+			
 			for (String SOAPServer : SOAPServers ) {
 				DocumentManagement aDocManClient = (new DocumentManagement_Service(new URL( SOAPServer + "DocumentManagement.svc"))).getBasicHttpBindingDocumentManagement();
 				((WSBindingProvider) aDocManClient).setOutboundHeaders(Headers.create(otAuthElement));
 				this.docManClients.add(aDocManClient);
-
+				
 				ContentService aContentService = (new ContentService_Service(new URL( SOAPServer + "ContentService.svc"))).getBasicHttpBindingContentService(new MTOMFeature());
 				((WSBindingProvider) aContentService).setOutboundHeaders(Headers.create(otAuthElement));
 				this.contentServices.add(aContentService);
-
+				
 				PhysicalObjects aPhysicalObjectsService = (new PhysicalObjects_Service(new URL( SOAPServer + "PhysicalObjects.svc"))).getBasicHttpBindingPhysicalObjects();
 				((WSBindingProvider) aPhysicalObjectsService).setOutboundHeaders(Headers.create(otAuthElement));
 				this.physicalObjectsServices.add(aPhysicalObjectsService);
@@ -94,13 +96,13 @@ public class SOAPServices {
 		}
 		
 	}
-
+	
 	// Namespace for the SOAP headers
 	public final static String ECM_API_NAMESPACE = "urn:api.ecm.opentext.com";
-
+	
 	// store the Authentication token
 	private String authToken = null;
-
+	
 	// store the available Document_Management services
 	private List<DocumentManagement> docManClients = new ArrayList<DocumentManagement>();
 	
@@ -116,7 +118,7 @@ public class SOAPServices {
 	public ContentService contentService() {
 		return contentServices.get((new Random()).nextInt(contentServices.size()));
 	}
-
+	
 	// store the available PhysicalObjects services
 	private List<PhysicalObjects> physicalObjectsServices = new ArrayList<PhysicalObjects>();
 	
